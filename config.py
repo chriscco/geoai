8578da90d2def6cea229af41a60e76d141ba37f4
chriscco <2444628489c@gmail.com>
2024-07-16 17:17:15 +0800

    api_key updated

--- config.py
+++ config.py
@@ -1,9 +1,15 @@
-from elasticsearch import Elasticsearch, helpers
+from elasticsearch7 import Elasticsearch, helpers
 from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import nltk
 import re
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.environ.get("OPENAI_API_KEY")
 
 
 def setup_elasticsearch():
